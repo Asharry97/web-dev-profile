5554bdbcb7056f2c6fb628b7f83ba8849302d78c
Asharry97 <Asharry97@users.noreply.github.com>
2021-03-14 16:40:24 +0530

    Web Dev Portfolio Initial 0.5 + Resume 0.3

--- js/index.js
+++ js/index.js
@@ -1,54 +1,44 @@
-
-function fileLink() {
-    let this_pathname = window.location.pathname;
-    let this_origin = window.location.origin;
-    let filePath = this_pathname.replace(/(.*?)[^/]*\..*$/, '$1');
-    console.log("F ", filePath);
-    console.log("F ", this_origin);
-    console.log("F ", this_origin + filePath);
-    return this_origin + filePath;
+/*** Comments:  */
+function thisFilePath() {
+    let currhref = window.location.href;
+    let filePath = currhref.substring(0, currhref.lastIndexOf("/"));
+    return filePath;
 }
+
+const thisLoc = thisFilePath();
 
 // Get Navbar
 async function loadNavbar() {
-    const this_loc = fileLink();
-    document.querySelector(".navbar-component").innerHTML = await (await fetch(this_loc + '/navbar.html')).text();
+    document.querySelector(".navbar-component").innerHTML = await (await fetch(thisLoc + '/navbar.html')).text();
     setActiveLink();
 }
 
 // Get Footer
 async function loadFooter() {
-    const this_loc = fileLink();
-    document.querySelector(".footer-component").innerHTML = await (await fetch(this_loc + '/footer.html')).text();
+    document.querySelector(".footer-component").innerHTML = await (await fetch(thisLoc + '/footer.html')).text();
 }
 
 function setActiveLink() {
     console.log("window.location.href => ", window.location.href);
-    this_url = new URL(window.location.href);
-    this_window_path_file = this_url.pathname.split("/").pop();
+    let thisResFile = window.location.pathname.split("/").pop().split("#")[0].trim();
 
-    let nav_links = document.querySelectorAll("nav .navbar-nav .nav-link");
+    let navLinks = document.querySelectorAll("nav .navbar-nav .nav-link");
 
-    for (let index = 0; index < nav_links.length; index++) {
-        let one_nav_link = nav_links[index];
-        let this_nav_link_path_file = one_nav_link.href.split("/").pop().split("#")[0];
-        if (this_nav_link_path_file === this_window_path_file) {
-            console.log("this_nav_link_path_file: ", this_nav_link_path_file, "this_window_path_file: ", this_window_path_file, " NavLink: ", one_nav_link);
-            one_nav_link.classList.add("active");
-            break;
-        } else if (one_nav_link.textContent.trim().toLowerCase() === this_window_path_file.split(".")[0].toLowerCase()) {
-            console.log("this_nav_link_path_file: ", this_nav_link_path_file, "this_window_path_file: ", this_window_path_file, " NavLink: ", one_nav_link);
-            one_nav_link.classList.add("active");
-            break;
-        } else if (this_window_path_file.trim() === "" && this_nav_link_path_file === "index.html") {
-            console.log("this_nav_link_path_file: ", this_nav_link_path_file, "this_window_path_file: ", this_window_path_file, " NavLink: ", one_nav_link);
-            one_nav_link.classList.add("active");
+    for (oneNavLink of navLinks) {
+
+        let currResFile = oneNavLink.href.split("/").pop().trim();
+
+        if (thisResFile === "" && (oneNavLink.href === "#" || currResFile === "index.html")) {
+            oneNavLink.classList.add("active");
             break;
         }
-        console.log("this_nav_link_path_file: ", this_nav_link_path_file, "this_window_path_file: ", this_window_path_file, " NavLink: ", one_nav_link);
-    }
 
-    console.log("this_window_path_file => ", this_window_path_file);
+        if (thisResFile === currResFile) {
+            oneNavLink.classList.add("active");
+            break;
+        }
+
+    }
 }
 
 function main() {
